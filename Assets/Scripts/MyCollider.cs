c68ac4408dd70058aa92540e824c3084fba18d01
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2016-11-04 15:22:17 +0530

    Day 2....Added ball, gravity, 2D Physics, Rididbody, Kinematic, Paddle

---
+++ Assets/Scripts/MyCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log("Collision is occuring"); 
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		Debug.Log("Trigger is occuring"); 
+		levelManager.LoadLevel("LossMenu"); 
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		//Mathf.Clamp()
+
+	    Vector3 paddlePos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+		float mousePositionX = Input.mousePosition.x / Screen.width * 16;
+		mousePositionX = Mathf.Clamp(mousePositionX, 0.5f , 15.5f); 
+		Debug.Log(mousePositionX);
+
+		paddlePos.x = mousePositionX; 
+
+		this.transform.position = paddlePos;
+
+		//float mousePositionX = Input.mousePosition.x; 
+		//Vector3 currentPosition = new Vector3(mousePositionX, this.transform.position.y, this.transform.position.z );
+		//this.transform.position = currentPosition; 
+	}
+}
